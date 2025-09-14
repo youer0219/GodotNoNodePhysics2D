2b7ecda367db095873be0e2124b824e4f4913b53
youer0219 <xlm20040219@qq.com>
2025-09-14 20:43:22 +0800

    测试代码更新

--- test/test_global_area_manager/test_area_manager.gd
+++ test/test_global_area_manager/test_area_manager.gd
@@ -3,18 +3,12 @@ extends Node2D
 ## 需要改进:
 ## 1.在GDS测试稳定后，可以考虑转为C++编写，以获取更好性能和更底层改造
 ## 2.更多性能测试
-## 3.提供更多的调整Area的方法
-## 4.area的set_transform方法疑似无效，碰撞对对数在位移一段时间后无变化
-	## 未来可能设定一个node2d-instance类，但没有渲染部分
 
 ## NOTE: 目前area-instance需要在_on_test_area_2d_area_shape_entered中才能被精确发现
-## Area2D似乎也会被发现，但只会发射一次area-enter信号
+## 节点级的信号也会发射，但只会发射一次  ——————  TODO:或者不附加节点，直接为null会更好？
 
 ## 目前的碰撞对数高了也会很卡。需要管理好碰撞层级。
 	## 传统物理引擎5万以上（1000个相互检测的Area）时，帧率暴降；剑柄表现明显不错，1500个没有下降
-## 以及在必要时候禁用物理
-	## 比如，一个作用于5个子弹的子弹影响区域，有5个子弹进来后，就禁用碰撞。
-## NOTE: 区域和查询分离。仅在必要时使用区域。
 
 @export var area_data: QuickAreaData
 
@@ -27,7 +21,6 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	for instance:QuickAreaInstance in area_instances:
-		## TODO: 疑似失效
 		var instance_transform = Transform2D(0,instance.position + Vector2(10,10) * delta)
 		instance.set_transform(instance_transform)
 
