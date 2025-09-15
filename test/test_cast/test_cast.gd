ae8406df19b923c5b75e3a38b100437b382b6f66
youer0219 <xlm20040219@qq.com>
2025-09-15 10:59:27 +0800

    修复测试错误（使用静态物理体）

--- test/test_cast/test_cast.gd
+++ test/test_cast/test_cast.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var quick_ray_cast_instance:QuickRayCastInstance
+var query:PhysicsRayQueryParameters2D
+var state: PhysicsDirectSpaceState2D
 
 func _ready() -> void:
 	quick_ray_cast_instance = QuickRayCastInstance.new()
@@ -9,20 +11,20 @@ func _ready() -> void:
 	quick_ray_cast_instance.collision_mask = 1
 	quick_ray_cast_instance.hit_from_inside = true
 	quick_ray_cast_instance.target_position = Vector2.ZERO
-
-func _physics_process(_delta: float) -> void:
-	if get_tree().get_frame() % 30 == 0:
-		cast()
-		$RayCast2D.force_raycast_update()
-		print($RayCast2D.get_collider())
-
-func cast():
-	var query =  PhysicsRayQueryParameters2D.new()
+	
+	query =  PhysicsRayQueryParameters2D.new()
 	query.collide_with_areas = quick_ray_cast_instance.collide_with_areas
 	query.collide_with_bodies = quick_ray_cast_instance.collide_with_bodies
 	query.collision_mask = quick_ray_cast_instance.collision_mask
 	query.hit_from_inside = quick_ray_cast_instance.hit_from_inside
 	query.from = quick_ray_cast_instance.position
 	query.to = quick_ray_cast_instance.position + quick_ray_cast_instance.target_position
-	var result = PhysicsServer2D.space_get_direct_state(get_world_2d().space).intersect_ray(query)
+	
+	state = PhysicsServer2D.space_get_direct_state(get_world_2d().space)
+
+func _physics_process(_delta: float) -> void:
+	cast()
+
+func cast():
+	var result = state.intersect_ray(query)
 	print(result)
