daa94582c4423f793efb208c0b8fd46d338613ba
youer0219 <xlm20040219@qq.com>
2025-11-08 23:06:53 +0800

    简化了 GlobalAreaManager： - 移除了池化和删除Area相关代码 - 提供清晰的注册/注销接口

--- addons/no_node_physics_2d/area/global_area_manager.gd
+++ addons/no_node_physics_2d/area/global_area_manager.gd
@@ -1,62 +1,45 @@
+# GlobalAreaManager.gd
 extends Node2D
 
 var instances: Dictionary = {}  # RID -> QuickAreaInstance
 var space: RID
-var instance_pool: Array[QuickAreaInstance] = []
-
-@export var use_pool: bool = true:
-	set(value):
-		use_pool = value
-		if not use_pool:
-			clear_pool_instances()
 
 func _ready() -> void:
 	space = get_world_2d().space
 
-func add_area(area_data: QuickAreaData, area_owner: Object, area_transform: Transform2D = Transform2D()) -> QuickAreaInstance:
-	var instance: QuickAreaInstance
-	if use_pool and instance_pool.size() > 0:
-		instance = instance_pool.pop_back() as QuickAreaInstance
-		instance.setup(area_data, area_owner, area_transform, space)
-	else:
-		instance = QuickAreaInstance.new(area_data, area_owner, area_transform, space)
-		#PhysicsServer2D.area_attach_object_instance_id(instance.area_rid, get_instance_id()) ## 不再附加节点。直接为null。
-	
+# 注册AreaInstance
+func register_instance(instance: QuickAreaInstance) -> void:
 	instances[instance.area_rid] = instance
-	return instance
 
-func remove_area(area_rid: RID) -> void:
-	if not instances.has(area_rid):
-		return
-	var instance: QuickAreaInstance = instances[area_rid]
-	
-	if use_pool:
-		instance.clear_for_pool()
-		instance_pool.append(instance)
-	else:
-		instance.free_rids()
-	
+# 注销AreaInstance
+func unregister_instance(area_rid: RID) -> void:
 	instances.erase(area_rid)
 
-func clear_active_instances():
-	for instance:QuickAreaInstance in instances.values():
+# 创建新的AreaInstance
+func create_area(area_data: QuickAreaData, area_owner: Object, area_transform: Transform2D = Transform2D()) -> QuickAreaInstance:
+	return QuickAreaInstance.new(area_data, area_owner, area_transform, space)
+
+# 根据RID获取AreaInstance的所有者
+func get_area_owner(area_rid: RID) -> Object:
+	var instance = instances.get(area_rid)
+	return instance.get_owner() if instance else null
+
+# 根据RID获取AreaInstance
+func get_area_instance(area_rid: RID) -> QuickAreaInstance:
+	return instances.get(area_rid)
+
+# 设置AreaInstance的变换
+func set_area_transform(area_rid: RID, xform: Transform2D) -> void:
+	var instance = instances.get(area_rid)
+	if instance:
+		instance.set_transform(xform)
+
+# 获取所有活跃的AreaInstance数量（调试用）
+func get_active_instance_count() -> int:
+	return instances.size()
+
+# 清理所有实例（主要用于场景切换）
+func clear_all_instances() -> void:
+	for instance in instances.values():
 		instance.free_rids()
 	instances.clear()
-
-func clear_pool_instances():
-	for inst in instance_pool:
-		inst.free_rids()
-	instance_pool.clear()
-
-func clear() -> void:
-	clear_active_instances()
-	clear_pool_instances()
-
-func get_area_owner(area_rid:RID)->Object:
-	if instances.has(area_rid):
-		return instances[area_rid].get_owner()
-	return null
-
-func set_area_transform(area_rid: RID, xform: Transform2D) -> void:
-	if instances.has(area_rid):
-		instances[area_rid].set_transform(xform)
