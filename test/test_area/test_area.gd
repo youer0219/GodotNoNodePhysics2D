6715793e8c9162bcad1e5cff98a5ba303add24e5
youer0219 <xlm20040219@qq.com>
2025-11-09 11:08:23 +0800

    测试：实例销毁和内存管理测试

--- test/test_area/test_area.gd
+++ test/test_area/test_area.gd
@@ -7,17 +7,26 @@ func _ready() -> void:
 	base_test()
 
 func base_test():
-	## 实例创建与销毁
+	## 实例创建测试
 	var data := base_area_data.duplicate()
-	# QuickAreaInstance.new(data,self) 这个的生成是否应该强制使用全局管理呢？
+	# TODO: QuickAreaInstance.new(data,self) 这个的生成是否应该强制使用全局管理呢？
 	var instance := GlobalAreaManager.create_area(data,self)
 	var area_rid := instance.area_rid
+	var shape_rid := instance.shape_rid
 	
 	var finded_instance := GlobalAreaManager.get_area_instance(area_rid)
-	if finded_instance and finded_instance.get_owner() == self:
+	if finded_instance and finded_instance.get_owner() == self and finded_instance.shape_rid == shape_rid:
 		print("OK")
 	
-	# 直接将instance设置为null无法销毁instance，因为全局管理中其被设置为强引用
-	# 需要将其改为弱引用
+	## 实例销毁测试
+	instance = null
+	finded_instance = null
+	finded_instance = GlobalAreaManager.get_area_instance(area_rid)
+	if finded_instance and finded_instance.get_owner() == self:
+		print("NOT OK")
 	
-	pass
+	## 判断area-rid和shape-rid被正确释放 注意报错
+	var shape_count := PhysicsServer2D.area_get_shape_count(area_rid)
+	print(shape_count == -1)
+	var shape_data = PhysicsServer2D.shape_get_data(shape_rid)
+	print(shape_data == null)
