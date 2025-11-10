e6214066cfe6431327e7089d596a700f3364be92
youer0219 <xlm20040219@qq.com>
2025-11-10 22:38:41 +0800

    添加新的area相关测试

--- addons/no_node_physics_2d/no_node_physics_factory.gd
+++ addons/no_node_physics_2d/no_node_physics_factory.gd
@@ -1,7 +1,7 @@
 # ============================================================================
 #  no_node_physics_factory.gd
 # ---------------------------------------------------------------------------
-#  物理实体工厂，负责创建 Area2D、RayCast2D、ShapeCast2D 等物理实体
+#  物理实体工厂，负责创建 Area2D、RayCast2D、ShapeCast2D 等非节点物理实例
 #  作为自动加载单例使用，实例自行管理生命周期
 # ============================================================================
 #class_name NoNodePhysicsFactory
@@ -18,7 +18,7 @@ func create_area(area_data: QuickAreaData, area_owner: Object, area_transform: T
 	return QuickAreaInstance.new(area_data, area_owner, area_transform, space)
 
 # 根据 RID 获取 AreaInstance
-func get_area_by_rid(rid: RID) -> QuickAreaInstance:
+func get_area_instance_by_rid(rid: RID) -> QuickAreaInstance:
 	var id = PhysicsServer2D.area_get_object_instance_id(rid)
 	var instance = instance_from_id(id)
 	return instance as QuickAreaInstance
