ed605b865fe1006f099fed4069de9353eeaf0ba1
youer0219 <xlm20040219@qq.com>
2025-11-10 15:06:10 +0800

    文件架构调整、删除弃用的代码

--- addons/no_node_physics_2d/instance/cast/quick_ray_cast_instance.gd
+++
@@ -1,6 +0,0 @@
-extends QuickCastInstance
-class_name QuickRayCastInstance
-
-## Ray专属
-var hit_from_inside:bool
-var query:PhysicsRayQueryParameters2D
--- addons/no_node_physics_2d/instance/cast/quick_shape_cast_instance.gd
+++
@@ -1,8 +0,0 @@
-extends QuickCastInstance
-class_name QuickShapeCastInstance
-
-## Shape专属
-var margin:float
-var max_results:int
-var shape:Shape2D
-var query:PhysicsShapeQueryParameters2D
