0fdf399528dc976ac2443fb3b7f2278f13b1208c
youer0219 <xlm20040219@qq.com>
2025-11-15 23:00:36 +0800

    ray-cast：实现射线实例属性和构造方法 - 等后续细化后再研究“私有”属性和一个和shape-cast乃至area公用的方法

--- addons/no_node_physics_2d/src/cast/ray/quick_ray_cast_data.gd
+++ addons/no_node_physics_2d/src/cast/ray/quick_ray_cast_data.gd
@@ -4,7 +4,7 @@ class_name QuickRayCastData
 @export var collide_with_areas:bool = false
 @export var collide_with_bodies:bool = true
 @export var collision_mask:int = 1
-@export var enable:bool = true
+@export var enabled:bool = true
 @export var exclude_parent:bool = true
 @export var hit_from_inside:bool = true
 @export var target_position:Vector2 = Vector2(0,50)
--- addons/no_node_physics_2d/src/cast/ray/quick_ray_cast_instance.gd
+++ addons/no_node_physics_2d/src/cast/ray/quick_ray_cast_instance.gd
@@ -1,2 +1,51 @@
 extends Instance2D
 class_name QuickRayCastInstance
+
+var data:QuickRayCastData
+
+# 基本属性
+var enabled: bool = true
+var target_position: Vector2 = Vector2(0, 50)  # 默认向下50像素
+var collision_mask: int = 1
+var exclude_parent_body: bool = true
+var collide_with_areas: bool = false
+var collide_with_bodies: bool = true
+var hit_from_inside: bool = false
+var exclude_parent:bool = true
+
+var space:RID = RID()
+var exclude_parent_rid:RID = RID()
+
+# 碰撞结果
+var collided: bool = false
+var against: Object = null
+var against_rid: RID
+var against_shape: int = 0
+var collision_point: Vector2 = Vector2.ZERO
+var collision_normal: Vector2 = Vector2.ZERO
+
+# 排除列表
+var exclude: Array[RID] = []
+
+var _space_state: PhysicsDirectSpaceState2D = null
+
+# 构造函数
+func _init(
+	ray_cast_data:QuickRayCastData, ## Ray属性配置
+	space_state: PhysicsDirectSpaceState2D,
+	exclude_parent_collision_object_2d:CollisionObject2D = null ## 需要排除碰撞的“父”节点
+	) -> void:
+	enabled = ray_cast_data.enabled
+	target_position = ray_cast_data.target_position
+	collision_mask = ray_cast_data.collision_mask
+	collide_with_areas = ray_cast_data.collide_with_areas
+	collide_with_bodies = ray_cast_data.collide_with_bodies
+	hit_from_inside = ray_cast_data.hit_from_inside
+	exclude_parent = ray_cast_data.exclude_parent
+	
+	_space_state = space_state
+	
+	if exclude_parent_collision_object_2d:
+		exclude_parent_rid = exclude_parent_collision_object_2d.get_rid()
+		if exclude_parent:
+			exclude.append(exclude_parent_rid)
